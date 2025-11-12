466481dcb5ace5dc53e99b9fa11db8a9bbb3199d
Alex <alexx5039@gmail.com>
2025-11-12 19:47:00 +0200

    coin combination implemented

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,35 @@
+import pytest
 from app.main import get_coin_combination
 
 
-# write your tests here
+@pytest.mark.parametrize(
+    "cents, expected",
+    [
+        (0, [0, 0, 0, 0]),
+        (1, [1, 0, 0, 0]),
+        (5, [0, 1, 0, 0]),
+        (10, [0, 0, 1, 0]),
+        (25, [0, 0, 0, 1]),
+        (6, [1, 1, 0, 0]),
+        (17, [2, 1, 1, 0]),
+        (50, [0, 0, 0, 2]),
+        (99, [4, 0, 2, 3]),
+    ],
+)
+def test_should_return_correct_coin_combination(
+        cents: int,
+        expected: list
+) -> None:
+    assert get_coin_combination(cents) == expected
+
+
+@pytest.mark.parametrize("cents", [-1, -50, -999])
+def test_should_raise_error_for_negative_values(cents: int) -> None:
+    with pytest.raises(ValueError):
+        get_coin_combination(cents)
+
+
+@pytest.mark.parametrize("cents", [1.5, "10", None, [10]])
+def test_should_raise_type_error_for_invalid_input_types(cents: int) -> None:
+    with pytest.raises((TypeError, ValueError)):
+        get_coin_combination(cents)
